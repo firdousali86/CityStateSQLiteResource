77935be9ecdb2b1b4be603e9986aa893419047d5
Firdous <firdous.ali@tenpearls.com>
2014-04-30 19:01:57 +0500

    changes

--- StateCityExtraction/ViewController.m
+++ StateCityExtraction/ViewController.m
@@ -29,22 +29,29 @@
     NSString *Data = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil ];
     if (Data)
     {
-        NSArray *records = [Data componentsSeparatedByString:@",eol"];
+        NSArray *records = [Data componentsSeparatedByString:@",eotl"];
         NSInteger idx;
         for (idx = 0; idx < records.count; idx++) {
             //zip,type,primary_city,acceptable_cities,unacceptable_cities,state,county,timezone,area_codes,latitude,longitude,world_region,country,decommissioned,estimated_population
             NSString *data =[records objectAtIndex:idx];
             NSLog(@"DSFGLKJ SDFG: %@", data);
-            //NSArray *fields = [Data componentsSeparatedByString:@","];
+            NSArray *fields = [data componentsSeparatedByString:@","];
             
+            NSString *queryFormat = [NSString stringWithFormat:@"insert into rawData (zip,type,primary_city,acceptable_cities,unacceptable_cities,state,county,timezone,area_codes,latitude,longitude,world_region,country,decommissioned,estimated_population) VALUES(%@,%@,%@,%@,%@,%@,%@,%@,%@,%@,%@,%@,%@,%@,%@)", [self getQuotedString:[fields objectAtIndex:0]],[self getQuotedString:[fields objectAtIndex:1]],[self getQuotedString:[fields objectAtIndex:2]],[self getQuotedString:[fields objectAtIndex:3]],[self getQuotedString:[fields objectAtIndex:4]],[self getQuotedString:[fields objectAtIndex:5]],[self getQuotedString:[fields objectAtIndex:6]],[self getQuotedString:[fields objectAtIndex:7]],[self getQuotedString:[fields objectAtIndex:8]],[self getQuotedString:[fields objectAtIndex:9]],[self getQuotedString:[fields objectAtIndex:10]],[self getQuotedString:[fields objectAtIndex:11]],[self getQuotedString:[fields objectAtIndex:12]],[self getQuotedString:[fields objectAtIndex:13]],[self getQuotedString:[fields objectAtIndex:14]]];
+            
+            //NSString *query = @"insert into rawData (zip,type,primary_city,acceptable_cities,unacceptable_cities,state,county,timezone,area_codes,latitude,longitude,world_region,country,decommissioned,estimated_population) VALUES(%@,%@,%@,%@,%@,%@,%@,%@,%@,%@,%@,%@,%@,%@,%@,%@)";
+            [ZeeSQLiteHelper initializeSQLiteDB];
+            [ZeeSQLiteHelper executeQuery:queryFormat];
+            [ZeeSQLiteHelper closeDatabase];
         }
         
         
-        /*NSString *query = @"insert into rawData (zip,type,primary_city,acceptable_cities,unacceptable_cities,state,county,timezone,area_codes,latitude,longitude,world_region,country,decommissioned,estimated_population) VALUES('Deserts')";
-        [ZeeSQLiteHelper initializeSQLiteDB];
-        [ZeeSQLiteHelper executeQuery:query];
-        [ZeeSQLiteHelper closeDatabase];*/
+        
     }
+}
+
+- (NSString*)getQuotedString:(NSString*)string{
+    return [NSString stringWithFormat:@"'%@'", string];
 }
 
 - (void)didReceiveMemoryWarning
