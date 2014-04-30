bd19d8544ad73f0ff6bfc71c76e88b0b263e4c2e
Firdous <firdous.ali@tenpearls.com>
2014-04-30 10:50:32 +0500

    Initial Commit

---
+++ StateCityExtraction/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StateCityExtraction
+//
+//  Created by Firdous on 30/04/2014.
+//  Copyright (c) 2014 TenPearls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StateCityExtraction/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StateCityExtraction
+//
+//  Created by Firdous on 30/04/2014.
+//  Copyright (c) 2014 TenPearls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
